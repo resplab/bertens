1cd9edba61a12caaf9798693b9350f57227d5c24
Amin Adibi <adibi@alumni.ubc.ca>
2021-03-17 09:06:57 -0700

    abs diffs in tests now

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -1,8 +1,8 @@
 test_that("example patient 1 works", {
-  expect_lt(bertens(exacerbationHx = TRUE, fev1=32.9, packYears = 38, vascularDx = TRUE) - 0.79, 0.02)
+  expect_lt(abs(bertens(exacerbationHx = TRUE, fev1=32.9, packYears = 38, vascularDx = TRUE) - 0.79), 0.02)
 })
 
 
 test_that("example patient 2 works", {
-  expect_lt(bertens(exacerbationHx = FALSE, fev1=77.7, packYears = 0, vascularDx = FALSE) - 0.09, 0.02)
+  expect_lt(abs(bertens(exacerbationHx = FALSE, fev1=77.7, packYears = 0, vascularDx = FALSE) - 0.09), 0.02)
 })
